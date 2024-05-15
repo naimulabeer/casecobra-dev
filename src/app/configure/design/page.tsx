4d9bd9e9cae944c2fd629103473aeb906671bc03
naimulabeer <naimul.h.abeer@gmail.com>
2024-05-16 01:55:53 +0600

    creating a reusable component for design configurator

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,37 @@
-const Page = async () => {};
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageUrl={imageUrl}
+      imageDimension={{ width, height }}
+    />
+  );
+};
 
 export default Page;
