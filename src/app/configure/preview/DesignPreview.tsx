254f80214b9109e2b5e2782cc8230d9073c8ce82
naimulabeer <naimul.h.abeer@gmail.com>
2024-05-21 00:24:02 +0600

    payment with stripe added with fixing some bugs on user database

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -11,8 +11,14 @@ import { useMutation } from "@tanstack/react-query";
 import { ArrowRight, Check } from "lucide-react";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
+import { createCheckoutSession } from "./actions";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
   useEffect(() => setShowConfetti(true));
 
@@ -31,8 +37,20 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     totalPrice += PRODUCT_PRICES.material.polycarbonate;
   if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
 
-  const {} = useMutation({
+  const { mutate: createPaymentSession } = useMutation({
     mutationKey: ["get-checkout-session"],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({ url }) => {
+      if (url) router.push(url);
+      else throw new Error("Unable to retrieve payment URL.");
+    },
+    onError: () => {
+      toast({
+        title: "Something went wrong",
+        description: "There was an error on our end. Please try again.",
+        variant: "destructive",
+      });
+    },
   });
 
   return (
@@ -127,8 +145,9 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
             <div className="mt-8 flex justify-end pb-12">
               <Button
-                //onClick={() => handleCheckout()}
-
+                onClick={() =>
+                  createPaymentSession({ configId: configuration.id })
+                }
                 className="px-4 sm:px-6 lg:px-8"
               >
                 Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
--- src/app/configure/preview/actions.tsx
+++ src/app/configure/preview/actions.tsx
@@ -2,10 +2,11 @@
 
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/product";
 import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Order } from "@prisma/client";
 
-export const CreateCheckoutSession = async ({
+export const createCheckoutSession = async ({
   configId,
 }: {
   configId: string;
@@ -41,15 +42,41 @@ export const CreateCheckoutSession = async ({
     },
   });
 
+  console.log(user.id, configuration.id);
+
   if (existingOrder) {
     order = existingOrder;
   } else {
     order = await db.order.create({
       data: {
-        amount: price,
+        amount: price / 100,
         userId: user.id,
         configurationId: configuration.id,
       },
     });
   }
+
+  const product = await stripe.products.create({
+    name: "Custom iPhone Case",
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: "USD",
+      unit_amount: price,
+    },
+  });
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ["card"],
+    mode: "payment",
+    shipping_address_collection: { allowed_countries: ["BD", "US"] },
+    metadata: {
+      userId: user.id,
+      orderId: order.id,
+    },
+    line_items: [{ price: product.default_price as string, quantity: 1 }],
+  });
+
+  return { url: stripeSession.url };
 };
